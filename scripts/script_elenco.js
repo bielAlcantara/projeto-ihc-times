07356c792b98f23b766e2d8a7e974b0c8f26e415
Gabriel Soares <gabrieldears@gmail.com>
2022-06-14 23:28:27 -0300

    add time liverpool

--- scripts/script_elenco.js
+++ scripts/script_elenco.js
@@ -7,4 +7,28 @@ fetch('../times.json').then(times => times.json()).then(times => {
     const tituloTime = document.querySelector('.nome-time-titulo');
 
     tituloTime.innerHTML = objTime.name;
+
+    const divGoleiro = document.querySelector('.goleiro');
+    const divLatEsquerdo = document.querySelector('.lateral-esquerdo');
+    const divZagEsquerdo = document.querySelector('.zagueiro-esquerdo');
+    const divZagDireito = document.querySelector('.zagueiro-direito');
+    const divLatDireito = document.querySelector('.lateral-direito');
+    const divMeiaEsquerda = document.querySelector('.meia-esquerda');
+    const divMeiaCentral = document.querySelector('.meia-central');
+    const divMeiaDireita = document.querySelector('.meia-direita');
+    const divPontaEsquerda = document.querySelector('.ponta-esquerda');
+    const divCentroAvante = document.querySelector('.centroavante');
+    const divPontaDireita = document.querySelector('.ponta-direita');
+
+    divGoleiro.innerHTML = objTime.squad.goleiro;
+    divLatEsquerdo.innerHTML = objTime.squad.latEsquerdo;
+    divZagEsquerdo.innerHTML = objTime.squad.zagEsquerdo;
+    divZagDireito.innerHTML = objTime.squad.zagDireito;
+    divLatDireito.innerHTML = objTime.squad.latDireito;
+    divMeiaEsquerda.innerHTML = objTime.squad.meiaEsquerda;
+    divMeiaCentral.innerHTML = objTime.squad.meiaCentral;
+    divMeiaDireita.innerHTML = objTime.squad.meiaDireita;
+    divPontaEsquerda.innerHTML = objTime.squad.pontaEsquerda;
+    divCentroAvante.innerHTML = objTime.squad.centroavante;
+    divPontaDireita.innerHTML = objTime.squad.pontaDireita;
 });
